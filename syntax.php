c586a42202a4ac598e8abba18b14811c81d6aff8
Leonard Heyman <lenehey@gmail.com>
2026-04-23 15:08:19 -0600

    DokuWiki localopen syntax plugin

---
+++ syntax.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * DokuWiki Plugin localopen (Syntax Component)
+ *
+ * @license MIT
+ * @author  Leonard Heyman
+ */
+
+class syntax_plugin_localopen extends \dokuwiki\Extension\SyntaxPlugin
+{
+    public function getType()
+    {
+        return 'substition';
+    }
+
+    public function getPType()
+    {
+        return 'normal';
+    }
+
+    public function getSort()
+    {
+        return 299;
+    }
+
+    public function connectTo($mode)
+    {
+        $tag = preg_quote($this->getConf('tag'), '/');
+
+        $this->Lexer->addSpecialPattern(
+            '\[\[' . $tag . '>[^|\]]+\|[^]]+\]\]',
+            $mode,
+            'plugin_localopen'
+        );
+    }
+
+    public function handle($match, $state, $pos, Doku_Handler $handler)
+    {
+        $tag = preg_quote($this->getConf('tag'), '/');
+
+        preg_match('/\[\[' . $tag . '>([^|]+)\|([^]]+)\]\]/i', $match, $matches);
+
+        $path = str_replace('"', '', $matches[1]);
+        $title = $matches[2];
+
+        return [
+            'path'  => $path,
+            'title' => $title,
+        ];
+    }
+
+    public function render($mode, Doku_Renderer $renderer, $data)
+    {
+        if ($mode !== 'xhtml') return false;
+
+        $path  = $data['path'];
+        $title = hsc($data['title']);
+
+        $token = $this->getConf('token');
+        $port  = $this->getConf('port');
+
+        $url = 'http://127.0.0.1:' . $port . '/open?path=' . rawurlencode($path) . '&token=' . rawurlencode($token);
+
+        $href = hsc($url);
+        $title_attr = hsc($path);
+
+        $renderer->doc .= '<a class="localopen-link" title="' . $title_attr . '" href="' . $href . '" onclick="fetch(this.href,{mode:\'no-cors\'}); return false;">' . $title . '</a>';
+
+        return true;
+    }
+}
